f1c7977bcef55a5e875130b135a50dd9b17cd35b
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-12 20:41:37 +0100

    Add spec for person, spec pass

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -1,17 +1,23 @@
-describe('person', function(){
+describe('Person', function(){
+  var person;
   beforeEach(function() {
     person = new Person({
-      gender: 'Female',
-      age: 30
-    });
+      gender: 'female',
+      age: 30,
+      distance: 1500
+  });
   });
 
   it('should have a gender', function() {
-    expect(person.gender).toBe('Female');
+    expect(person.gender).toBe('female');
   });
 
   it('should have age', function() {
     expect(person.age).toBe(30);
+  });
+
+  it("person should have a noted distance", function()  {
+  expect(person.distance).toEqual(1500);
   });
 
 });
