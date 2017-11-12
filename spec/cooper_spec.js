e877ebcbf011b4a443131a97076f8d4ac44b2cfb
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-12 21:37:40 +0100

    Add and modify tests

---
+++ spec/cooper_spec.js
@@ -0,0 +1,33 @@
+describe('CooperTest', function() {
+  describe("Female", function() {
+    it("returns a result for a 30 y o female", function() {
+      expect(CooperTest({
+        age: 30,
+        gender: "female",
+        distance: 1500
+      })).toEqual("Below Average");
+    });
+    it("returns a result for a 13 y o male", function() {
+      expect(CooperTest({
+        age: 13,
+        gender: 'male',
+        distance: 2500
+      })).toEqual("Above Average");
+    });
+    it("returns a result for a 25 y o male", function() {
+      expect(CooperTest({
+        age: 25,
+        gender: 'male',
+        distance: 1500
+      })).toEqual("Poor..");
+    });
+    it("returns a result for a 55 y o 'female'", function() {
+      expect(CooperTest({
+        age: 55,
+        gender: 'female',
+        distance: 2300
+      })).toEqual("Excellent!");
+    });
+  });
+
+});
